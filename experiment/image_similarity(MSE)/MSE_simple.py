56797d1d344e1ec63877afb39fa1380aee377222
rodom <enjoying1018@naver.com>
2021-10-04 02:03:07 +0900

    image similarity (Mean Squared Error)

---
+++ experiment/image_similarity(MSE)/MSE_simple.py
@@ -0,0 +1,13 @@
+import numpy as np
+import cv2
+
+def mse(image1, image2):
+    error = np.sum((image1.astype("int")-image2.astype("int"))**2)
+    error /= int(image1.shape[0] * image1.shape[1])
+    return abs(error)
+
+sample1 = cv2.imread("./sample1_1280x720.PNG", cv2.IMREAD_UNCHANGED)
+sample2 = cv2.imread("./sample6_1280x720.PNG", cv2.IMREAD_UNCHANGED)
+error = mse(sample1,sample2)
+print(error)
+
