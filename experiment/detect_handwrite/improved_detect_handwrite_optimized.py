ee891d30cfa3ef87c0a9fd61997f738c7873421f
PEERING\Owner <foba1@naver.com>
2021-10-24 23:16:08 +0900

    Add function to detect final write.

--- experiment/detect_handwrite/improved_detect_handwrite_optimized.py
+++ experiment/detect_handwrite/improved_detect_handwrite_optimized.py
@@ -6,13 +6,63 @@ import copy
 
 
 def detect_difference(before_frame, after_frame):
-    error = np.sum((before_frame.astype("int")-after_frame.astype("int"))**2)
-    error /= int(before_frame.shape[0] * after_frame.shape[1])
-    error = abs(error)
-    if error > 4000:
-        return True
+    error = np.sum((before_frame.astype("int") - after_frame.astype("int"))**2)
+    error /= int(before_frame.shape[0] * before_frame.shape[1])
+    return abs(error)
+
+
+def get_dif(color1, color2):
+    if color1 is None and color2 is not None:
+        return color2
+    elif color1 is not None and color2 is None:
+        return color1
+    elif color1 is None and color2 is None:
+        return None
+    elif len(color1) == 3 and len(color2) == 3:
+        dif = 0
+        for i in range(len(color1)):
+            if color1[i] > color2[i]:
+                dif += color1[i] - color2[i]
+            else:
+                dif += color2[i] - color1[i]
+        return dif
     else:
-        return False
+        return None
+
+
+def slice_and_visualize_difference(before, after, div):
+    height, width, layers = before.shape
+
+    block_width = width // div
+    block_height = height // div
+
+    temp_after = before.copy()
+
+    for row in range(div):
+        for col in range(div):
+            temp_image1 = before[block_height * row:block_height * (row + 1),
+                          block_width * col:block_width * (col + 1)].copy()
+            temp_image2 = after[block_height * row:block_height * (row + 1),
+                          block_width * col:block_width * (col + 1)].copy()
+            temp_after[block_height * row:block_height * (row + 1),
+                block_width * col:block_width * (col + 1)] = visualize_difference(temp_image1, temp_image2)
+
+    return temp_after
+
+
+def visualize_difference(before, after):
+    height, width, layers = before.shape
+
+    temp_after = after.copy()
+
+    for row in range(height):
+        for col in range(width):
+            if get_dif(before[row, col], after[row, col]) > 50:
+                temp_after[row, col] = before[row, col].copy()
+            else:
+                temp_after[row, col] = [255, 255, 255]
+
+    return temp_after
 
 
 def save_pdf_from_video(input, slice):
@@ -27,6 +77,14 @@ def save_pdf_from_video(input, slice):
     # prev frame 저장하는 곳. 지금 당장은 첫 프레임 저장.
     ret, prev_frame = capture.read()
     height, width, layers = prev_frame.shape
+
+    # origin slide 저장하는 곳.
+    origin_slide_array = []
+    origin_slide_array.append(prev_frame)
+
+    # final slide 저장하는 곳.
+    final_slide_array = []
+    final_slide_array.append(prev_frame)
 
     # temp 비교를 위한 저장공간들
     # temp_origin_frame : 장면전환 후 첫 슬라이드(필기가 없는 . 흰화면)
@@ -78,15 +136,22 @@ def save_pdf_from_video(input, slice):
                     temp_image1 = temp_origin_frame[start_height:end_height, start_width:end_width].copy()
                     temp_image2 = frame[start_height:end_height, start_width:end_width].copy()
 
-                    if detect_difference(temp_image1, temp_image2):
+                    if detect_difference(temp_image1, temp_image2) > 4000:
                         diff_list[i][j] = 1
                         different_slice += 1
 
             # 아예 다른 슬라이드 발견(슬라이드전환)
             if different_slice >= 6:
+                exist = False
+                for image in origin_slide_array:
+                    if detect_difference(image, frame) < 50:
+                        exist = True
                 frame_array.append(prev_frame)
+                final_slide_array.append(prev_frame)
                 temp_origin_frame = frame
                 prev_frame = frame
+                if not exist:
+                    origin_slide_array.append(frame)
                 continue
             # 인접한 곳에서 필기가 되었는가 ? 떨어진 곳에 필기를 했는지 detect.
             # 문제점3 : 떨어진 곳에서 필기를 했으나 같은 필기의 내용인 경우.
@@ -95,7 +160,6 @@ def save_pdf_from_video(input, slice):
 
             # 수정 3-1원본, 필기본 비교해서, 나중에 그 부분이 erase되면, 그때는 최신 필기본 저장한다.
             erased = False
-
             for i in range(slice):
                 if erased:
                     break
@@ -117,6 +181,12 @@ def save_pdf_from_video(input, slice):
     del frame_array[0]
     save_pdf(frame_array, "video/result_pdf.pdf")
     print(len(frame_array))
+
+    if len(origin_slide_array) == len(final_slide_array):
+        temp_array = []
+        for i in range(len(origin_slide_array) - 1):
+            temp_array.append(slice_and_visualize_difference(origin_slide_array[i], final_slide_array[i + 1], 16))
+        save_pdf(temp_array, "video/detect_write_result.pdf")
 
     print("time: ", end=" ")
     print(time.time() - start)
