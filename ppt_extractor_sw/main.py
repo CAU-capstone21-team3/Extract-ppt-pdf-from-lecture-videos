c6d86f90eb4a65a5505144edfa8f2b75d98fdb04
PEERING\Owner <foba1@naver.com>
2021-11-18 11:15:29 +0900

    Add function to select part of slides.

--- ppt_extractor_sw/main.py
+++ ppt_extractor_sw/main.py
@@ -1,5 +1,6 @@
-import time
 from tkinter import *
+from PIL import ImageTk, Image
+import time
 import cv2
 import numpy as np
 import pyautogui
@@ -172,16 +173,18 @@ def update():
 def stop():
     global in_progress
     in_progress = False
-    temp_array = []
-    for i in frame_array:
-        temp_array.append(i[0])
-    save_pdf(temp_array, "result.pdf")
-    label.config(text="PDF 변환 완료")
+    label.config(text="녹화 종료")
+    print(len(frame_array))
+    if len(frame_array) > 0:
+        root.after(1000, destroy)
 
-# GUI
+def destroy():
+    root.destroy()
+
+# Record GUI
 root = Tk()
-root.title("PDF")
-root.geometry("200x250+1400+300")
+root.title("Record")
+root.geometry("200x250")
 root.resizable(False, False)
 
 label = Label(root, text="영역을 설정해주세요")
@@ -193,5 +196,94 @@ stop_button = Button(root, text="Stop", command=stop, width=10, height=2, padx=3
 set_area_button.pack(pady=10)
 start_button.pack()
 stop_button.pack(pady=10)
+
+root.mainloop()
+
+def prev_image():
+    global slide_info, current_slide, check_var, slide_label
+    slide_info[current_slide] = check_var.get()
+    if current_slide > 0:
+        current_slide = current_slide - 1
+        temp_image = frame_array[current_slide][0].copy()
+        temp_image = cv2.resize(temp_image, dsize=(width, height), interpolation=cv2.INTER_LINEAR)
+        img = cv2.cvtColor(temp_image, cv2.COLOR_BGR2RGB)
+        img = Image.fromarray(img)
+        img = ImageTk.PhotoImage(image=img)
+        slide_label.config(image=img)
+        slide_label.image = img
+        check_var.set(slide_info[current_slide])
+        checkbox.update()
+
+def next_image():
+    global slide_info, current_slide, check_var, slide_label
+    slide_info[current_slide] = check_var.get()
+    if current_slide + 1 < len(frame_array):
+        current_slide = current_slide + 1
+        temp_image = frame_array[current_slide][0].copy()
+        temp_image = cv2.resize(temp_image, dsize=(width, height), interpolation=cv2.INTER_LINEAR)
+        img = cv2.cvtColor(temp_image, cv2.COLOR_BGR2RGB)
+        img = Image.fromarray(img)
+        img = ImageTk.PhotoImage(image=img)
+        slide_label.config(image=img)
+        slide_label.image = img
+        check_var.set(slide_info[current_slide])
+        checkbox.update()
+
+def save():
+    slide_info[current_slide] = check_var.get()
+    temp_array = []
+    for i in range(len(frame_array)):
+        if slide_info[i] == 1:
+            temp_array.append(frame_array[i][0])
+    save_pdf(temp_array, "result.pdf")
+    root.destroy()
+
+def calculate_slide_size():
+    temp_height, temp_width, layer = frame_array[0][0].shape
+    if temp_height / temp_width > 9 / 16:
+        ratio = 360 / temp_height
+        return int(temp_width * ratio), int(temp_height * ratio)
+    else:
+        ratio = 640 / temp_width
+        return int(temp_width * ratio), int(temp_height * ratio)
+
+# PDF Convert GUI
+current_slide = 0
+slide_info = []
+width, height = calculate_slide_size()
+for i in range(len(frame_array)):
+    slide_info.append(1)
+
+root = Tk()
+
+first_slide = frame_array[0][0].copy()
+first_slide = cv2.resize(first_slide,dsize=(width, height), interpolation=cv2.INTER_LINEAR)
+first_slide = cv2.cvtColor(first_slide, cv2.COLOR_BGR2RGB)
+first_slide = Image.fromarray(first_slide)
+first_slide = ImageTk.PhotoImage(image=first_slide)
+
+root.title("PDF")
+root.geometry("740x500")
+root.resizable(False, False)
+
+label = Label(root, text="PDF 변환 슬라이드 목록")
+label.grid(row=0, column=1, pady=10)
+
+convert_button = Button(root, text="저장", command=save, padx=5, pady=2)
+convert_button.grid(row=0, column=2)
+
+slide_label = Label(root, image=first_slide)
+slide_label.grid(row=1, column=1)
+
+prev_button = Button(root, text="<", command=prev_image, padx=5, pady=2)
+prev_button.grid(row=1, column=0, padx=10)
+
+next_button = Button(root, text=">", command=next_image, padx=5, pady=2)
+next_button.grid(row=1, column=2, padx=10)
+
+check_var = IntVar()
+checkbox = Checkbutton(root, text="슬라이드 포함", variable=check_var)
+checkbox.select()
+checkbox.grid(row=2, column=1)
 
 root.mainloop()
